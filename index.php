37a581069786ff8f06e9fbde6cb6b95b1135e6ee
Chethan Rao <70657455+Chethan-rao@users.noreply.github.com>
2021-08-30 12:41:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -77,28 +77,6 @@
 
 
 
-<section class="corona_update">
-	<div class="mb-3" style="margin-top: 40px">
-		<h3 class="text-uppercase text-center">covid-19 updates</h3>
-	</div>
-	<div class="d-flex justify-content-around align-items-center count_style">
-		<div>
-			<h1 class="count">512</h1>
-			<p>Active COVID-19 cases*</p>
-		</div>
-
-		<div>
-			<h1 class="count">40</h1>
-			<p>Discharged cases</p>
-		</div>
-
-		<div>
-			<h1 class="count">9</h1>
-			<p>Death cases</p>
-		</div>
-
-	</div>
-</section>
 
 
 <div class="container-fluid sub_section pt-5 pb-5" id="aboutid">
